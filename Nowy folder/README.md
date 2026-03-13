8ce1ac27508c296b626ef15d3ebf1563bc20dd99
skibidi12384 <krolpatyczkow@gmail.com>
2026-03-13 10:32:30 +0100

    Add files via upload

---
+++ Nowy folder/README.md
@@ -0,0 +1,2 @@
+# tsiai3a
+Nauka html css js i php
---
+++ Nowy folder/script.js
@@ -0,0 +1,15 @@
+const formularz = document.querySelector("#formularz")
+
+formularz.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    const imie = formularz.querySelector("#imie")
+    const nazwisko = formularz.querySelector("#nazwisko")
+    const email = formularz.querySelector('input[name="email"]')
+
+    console.log(imie.value, nazwisko.value, email.value)
+    imie.value = ""
+    nazwisko.value = ""
+    email.value = ""
+})
+
